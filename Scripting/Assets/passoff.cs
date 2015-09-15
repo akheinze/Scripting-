314f81fe825c333d361ccaa90752fce7b15908aa
angela heinze <angela.heinze@hotmail.com>
2015-09-15 08:56:36 -0600

    second commit!

---
+++ Scripting/Assets/passoff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class passoff : MonoBehaviour
+{   
+	int myInt = 21;
+	
+	
+	void Start ()
+	{
+		myInt = SubtractByThree(myInt);
+		Debug.Log (myInt);
+	}
+	
+	
+	int SubtractByThree (int number)
+	{
+		int ret;
+		ret = number - 3;
+		return ret;
+	}
+}
---
+++ Scripting/Assets/varablesandfunctions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pasoff : MonoBehaviour
+{   
+
+	int myInt = 21;
+	
+	
+	void Start ()
+	{
+		myInt = DivideByThree(myInt);
+		Debug.Log (myInt);
+	}
+
+
+	
+	int DivideByThree (int number)
+	{
+		int ret;
+		ret = number / 3;
+		return ret;
+	}
+}
